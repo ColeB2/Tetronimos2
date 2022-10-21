f83150b4929e5cdbe35035df5d59e29ba5f06bf9
Cole <cebCole@gmail.com>
2022-10-21 07:59:35 -0500

    __test__ with block, piece test files

---
+++ __tests__/block.test.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+test('test example', () => {
+    expect(3).toBe(3)
+})
+
+const Block = require('../src/components/block.ts')
+
+describe("Block", () => {
+    const block = new Block.Block(0,0,30,"red", 1);
+    console.log(block)
+
+    test("Block.x", () => {
+        expect(block.x).toBe(0);
+    })
+    test("Block.y", () => {
+        expect(block.y).toBe(0);
+    })
+    test("Block.width", () => {
+        expect(block.width).toBe(30);
+    })
+    test("Block.color", () => {
+        expect(block.color).toBe("red");
+    })
+    test("Block.state", () => {
+        expect(block.state).toBe(1);
+    })
+
+
+})
