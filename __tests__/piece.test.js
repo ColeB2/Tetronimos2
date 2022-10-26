ddffe6631f89bc05d98b8114fedc0c3cb2c914ea
Cole <cebCole@gmail.com>
2022-10-26 14:44:20 -0500

    removed tests that are failing, that arent completed

--- __tests__/piece.test.js
+++ __tests__/piece.test.js
@@ -8,18 +8,17 @@ const Board = require('../src/components/board.ts')
 
 const { vitalsMock } = jest.createMockFromModule("../src/components/tetronimo.ts")
 const { boardMock } = jest.createMockFromModule("../src/components/board.ts")
-console.log(boardMock.width)
 
 describe("Piece", () => {
     // const board = new Board.Board(10,10,"black", [])
     // const board = new boardMock.Board(10,10,"black", [])
-    const mockVitals = jest.mock("../src/components/tetronimo.ts")
+    // const mockVitals = jest.mock("../src/components/tetronimo.ts")
     const piece = new Piece.Piece(Vitals.tetronimoPieces[0], boardMock);
-    console.log("boardMock", boardMock)
-    console.log(Vitals.tetronimoPieces)
-    console.log(vitalsMock)
-    console.log(mockVitals)
-    console.log(piece)
+    // console.log("boardMock", boardMock)
+    // console.log(Vitals.tetronimoPieces)
+    // console.log(vitalsMock)
+    // console.log(mockVitals)
+    // console.log(piece)
 
     // test("Create Piece", () => {
     //     expect(  
