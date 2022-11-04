4bbf4ba8b1ec0f03d446267f83e2c64fdc851df4
Cole <cebCole@gmail.com>
2022-11-04 08:01:57 -0500

    more piece tests

--- __tests__/piece.test.js
+++ __tests__/piece.test.js
@@ -10,25 +10,48 @@ const { vitalsMock } = jest.createMockFromModule("../src/components/tetronimo.ts
 const { boardMock } = jest.createMockFromModule("../src/components/board.ts")
 
 describe("Piece", () => {
-    // const board = new Board.Board(10,10,"black", [])
-    // const board = new boardMock.Board(10,10,"black", [])
-    // const mockVitals = jest.mock("../src/components/tetronimo.ts")
-    const piece = new Piece.Piece(Vitals.tetronimoPieces[0], boardMock);
-    // console.log("boardMock", boardMock)
-    // console.log(Vitals.tetronimoPieces)
-    // console.log(vitalsMock)
-    // console.log(mockVitals)
-    // console.log(piece)
 
-    // test("Create Piece", () => {
-    //     expect(  
-    // })
-    test("Piece.orientation", () => {
-        expect(piece.orientation).toBe(0)
+    const piece_selected = Vitals.tetronimoPieces[2] // s piece
+    const s_piece = new Piece.Piece(piece_selected, boardMock);
+
+
+    test("Piece.name properly set", () => {
+        expect(s_piece.name).toBe(piece_selected.name)
     })
 
-    test("Piece.landed", () => {
-        expect(piece.landed).toBe(false);
+    test("Piece.color properly set", () => {
+        expect(s_piece.color).toBe(piece_selected.color)
+    })
+
+    test("Piece.allOrientations properly set", () => {
+        expect(s_piece.allOrientations).toBe(piece_selected.pieceMap)
+    })
+
+    test("Piece.orientation starts initial value of 0", () => {
+        expect(s_piece.orientation).toBe(0)
+    })
+
+    test("Piece.currentOrientation properly set", () => {
+        expect(s_piece.currentOrientation).toBe(s_piece.allOrientations[s_piece.orientation])
+    })
+
+    test("Piece.board points to proper boardObject", () => {
+        expect(s_piece.board).toBe(boardMock)
+    })
+
+    test("Piece.landed properly deaults to false", () => {
+        expect(s_piece.landed).toBe(false);
+    })
+
+    //Piece Specific Relations. sPiece
+    const sPieceWidth = 4 //sPiece Width = s_piece.currentOrientation[0].length
+    const sPieceXOffset = s_piece.xOffsetSpawnValue - sPieceWidth
+    test("piece.xOffset starts initial value of 0", () => {
+        expect(s_piece.xOffset).toBe(sPieceXOffset)
+    })
+
+    test("piece.yOffset starts initial value of 0", () => {
+        expect(s_piece.yOffset).toBe(1)
     })
 
     
